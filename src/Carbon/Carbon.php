7ea32bd0756d317e0f82404884ca196a71f31184
autumn.dev <sdjones1985@gmail.com>
2017-07-10 11:39:13 +0100

    Correct ISO8601 string format

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -1310,7 +1310,7 @@ class Carbon extends DateTime
      */
     public function toIso8601String()
     {
-        return $this->toAtomString();
+        return $this->format(static::ISO8601);
     }
 
     /**
